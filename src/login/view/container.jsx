f5b8156b711449a112efc59586d4b223172dcf78
c <c@cdeMacBook-Air.local>
2019-07-05 20:43:29 +0800

    实现校验工号输入不是六位数字时的提示功能

--- src/login/view/container.jsx
+++ src/login/view/container.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { List, InputItem, WhiteSpace, Button, WingBlank, Toast } from 'antd-mobile'
 import { createForm } from 'rc-form'
+import {get, post, put } from '../../utils/request'
+
+const headers = {'User-Agent': 'PostmanRuntime/7.13.0',
+                 'Accept':'*/*',
+                 'mode': 'no-cors',
+                 'Content-Type': 'application/json',
+                 'Cache-Control': 'no-cache',
+                 'Host': 'localhost:5000'}
 
 class Login extends React.Component {
   constructor (props) {
@@ -18,7 +26,8 @@ class Login extends React.Component {
         rule: {
           required: true,
           message: '请输入密码!'
-        }
+        },
+      hasError: false,
       }
     }
   }
@@ -42,6 +51,7 @@ class Login extends React.Component {
 
   handleSubmit = (e) => {
     let errors = []
+    let account = {}
     const { getFieldError, validateFields } = this.props.form
     e.preventDefault()
     validateFields((error, value) => {
@@ -51,14 +61,49 @@ class Login extends React.Component {
       }
       if (errors.length > 0) {
         this._showToast(errors.join('\n'), 1)
+      } else if (!this.state.hasError) {
+      for (const field of Object.keys(this.state)) {
+        const value = this._loadFromLocalStorage(field)
+        const result = {}
+        result[field] = value
+        account = {...account, ...result}
+      }
+        post('http://localhost:5000', headers, account)
       }
     })
+  }
+
+  handleErrorClick = () => {
+    if (this.state.hasError) {
+      this._showToast('Please enter 6 digits.', 1)
+    }
+  }
+
+  handleChange = (value) => {
+    if (value.replace(/\s/g, '').length != 6) {
+      this.setState({
+        hasError: true
+      })
+    } else if (!(/^\d+$/.test(value))){
+      this.setState({
+        hasError: true
+      })
+    } else {
+      this.setState({
+        hasError: false
+      })
+    }
   }
 
   _saveToLocalStorage (name, value) {
     if (value !== undefined) {
       localStorage.setItem(name, value)
     }
+  }
+
+  _loadFromLocalStorage (name) {
+    const value = localStorage.getItem(name)
+    return value
   }
 
   _showToast (information, time) {
@@ -71,20 +116,23 @@ class Login extends React.Component {
       <WingBlank>
         <WhiteSpace />
         <List renderHeader={() => '登录'}>
-          {this.decoratorFactory(jobNumber.field, jobNumber.rule)
-            (<InputItem
-               placeholder='job number'
-               onBlur={this.handleOnBlur}
-             >工号
+          {this.decoratorFactory(jobNumber.field, jobNumber.rule)(
+            <InputItem
+              error={this.state.hasError}
+              onErrorClick={this.handleErrorClick}
+              onChange={this.handleChange}
+              placeholder='job number'
+              onBlur={this.handleOnBlur}
+            >工号
           </InputItem>)}
           <WhiteSpace />
-        {this.decoratorFactory(password.field, password.rule)(
-          <InputItem
+          {this.decoratorFactory(password.field, password.rule)(
+            <InputItem
               type='password'
               placeholder='****'
               onBlur={this.handleOnBlur}
-            >密码
-          </InputItem>)}
+              >密码
+            </InputItem>)}
         </List>
         <WhiteSpace />
         <Button
