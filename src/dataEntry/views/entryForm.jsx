62921c2e39261ad6d433b8eacebb23170a9e56c4
c <7160844@qq.com>
2019-11-15 00:52:30 +0800

    将 entryForm.jsx 中的搜索组件提取成独立文件 search.jsx, 使用它时用 rc-form 中的 createForm 高阶组件对 Search 组件进行包裹

--- src/dataEntry/views/entryForm.jsx
+++ src/dataEntry/views/entryForm.jsx
@@ -1,92 +1,23 @@
 import React from 'react'
 import { formShape } from 'rc-form'
 import createForm from 'rc-form/lib/createDOMForm'
-import { Form, Search, Label, Grid } from 'semantic-ui-react'
+import { Form, Grid } from 'semantic-ui-react'
 
-import PropTypes from 'prop-types'
 import _ from 'lodash'
 
 import DatePicker from './datepicker'
+import Search from './search'
 
 import './style.css'
-
-const resultRenderer = ({ title }) => <Label content={title} />
-
-resultRenderer.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string
-}
-
-const initialState = { isLoading: false, results: [], value: '', open: false, segmentations: [] }
 
 class OriginForm extends React.Component {
   static propTypes = {
     form: formShape,
   };
 
-  state = initialState
-
   componentDidMount () {
     const datasheet = this.props.datasheet
     this.props.form.setFieldsValue(datasheet)
-  }
-
-  handleResultSelect = (e, { result }) => {
-    this.setState({ value: result.title, open: false, })
-    this.setSelected(result.title)
-  }
-
-  handleSearchChange = (e, { value }) => {
-    this.setState({ isLoading: true, value })
-    setTimeout(() => this.handleSegment(this.state.value))
-
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.setState(initialState)
-
-      const isMatch = result =>  this.state.segmentations.every((item, index, array) => {
-          return new RegExp(item).test(result.title)
-      })
-
-      this.setState({
-        isLoading: false,
-        results: _.filter(this.props.source, isMatch)
-      })
-
-    }, 500)
-
-    this.setSelected(value)
-  }
-
-  handleBlurSearch = (e) => {
-    if (!e.relatedTarget) { var open = false}
-    else if (e.relatedTarget && e.relatedTarget.classList) {
-      open = _.includes(e.relatedTarget.classList, 'result');
-    }
-    this.setState({ open: open, focused: false, });
-  }
-
-  handleFocusSearch = (e) => {
-    this.setState({ open: true, focused: true, });
-  }
-
-  handleSegment = (search) => {
-  const segmentationApi = 'http://182.61.145.178:3000/stage/api/segmentations/';
-  fetch(segmentationApi, {
-    method: 'POST',
-    headers: {
-    'Content-type': 'application/json'
-    },
-    body: JSON.stringify({search})
-  })
-  .then((response) => (response.json())
-    .then((responseJsonData) => {
-        console.log(responseJsonData)
-        this.setState({segmentations:responseJsonData.filter((item, index, array) => (item.length > 1))})
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-   )
   }
 
   handleStash = (event) => {
@@ -119,16 +50,9 @@ class OriginForm extends React.Component {
     this.props.form.resetFields();
   }
 
-  setSelected = (value) => {
-    this.props.form.setFieldsValue({
-      selected: value,
-    })
-  }
-
   render() {
     const { onStash, onDelete, formID, isOpen , ...rest } = this.props;
     const { getFieldDecorator, getFieldError } = this.props.form;
-    const { isLoading, results } = this.state;
     const fields = this.props.fields;
     const datasheet = this.props.datasheet;
     const value = datasheet && datasheet.date ? datasheet.date : null;
@@ -171,17 +95,6 @@ class OriginForm extends React.Component {
                         message: fields.selected.message,
                       }],
                     })(<Search
-                         loading={isLoading}
-                         onResultSelect={this.handleResultSelect}
-                         onSearchChange={_.debounce(this.handleSearchChange, 500, {
-                           leading: true
-                         })}
-                         results={results}
-                         resultRenderer={resultRenderer}
-                         placeholder={fields.selected.placeholder}
-                         open={this.state.open}
-                         onFocus={this.handleFocusSearch}
-                         onBlur={this.handleBlurSearch}
                          {...rest}
                     />)}
                     <div style={{ color: 'red' }}>
---
+++ src/dataEntry/views/search.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Search, Label } from 'semantic-ui-react'
+
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+
+const resultRenderer = ({ title }) => <Label content={title} />
+
+resultRenderer.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string
+}
+
+const initialState = { isLoading: false, results: [], value: '', open: false, segmentations: [] }
+
+class MySearch extends React.Component {
+  state = initialState
+
+  handleResultSelect = (e, { result }) => {
+    this.setState({ value: result.title, open: false, })
+    this.setSelected(result.title)
+  }
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+    setTimeout(() => this.handleSegment(this.state.value))
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.setState(initialState)
+
+      const isMatch = result =>  this.state.segmentations.every((item, index, array) => {
+          return new RegExp(item).test(result.title)
+      })
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(this.props.source, isMatch)
+      })
+
+    }, 500)
+
+    this.setSelected(value)
+  }
+
+  handleBlurSearch = (e) => {
+    if (!e.relatedTarget) { var open = false}
+    else if (e.relatedTarget && e.relatedTarget.classList) {
+      open = _.includes(e.relatedTarget.classList, 'result');
+    }
+    this.setState({ open: open, focused: false, });
+  }
+
+  handleFocusSearch = (e) => {
+    this.setState({ open: true, focused: true, });
+  }
+
+  handleSegment = (search) => {
+  const segmentationApi = 'http://182.61.145.178:3000/stage/api/segmentations/';
+  fetch(segmentationApi, {
+    method: 'POST',
+    headers: {
+    'Content-type': 'application/json'
+    },
+    body: JSON.stringify({search})
+  })
+  .then((response) => (response.json())
+    .then((responseJsonData) => {
+        console.log(responseJsonData)
+        this.setState({segmentations:responseJsonData.filter((item, index, array) => (item.length > 1))})
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+   )
+  }
+
+  setSelected = (value) => {
+    this.props.form.setFieldsValue({
+      selected: value,
+    })
+  }
+
+  render () {
+    const { placeholder , ...rest } = this.props;
+    const { isLoading, results } = this.state;
+
+    return (
+      <Search
+        loading={isLoading}
+        onResultSelect={this.handleResultSelect}
+        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+          leading: true
+        })}
+        results={results}
+        resultRenderer={resultRenderer}
+        placeholder={placeholder}
+        open={this.state.open}
+        onFocus={this.handleFocusSearch}
+        onBlur={this.handleBlurSearch}
+        {...rest}
+      />
+    )
+  }
+}
+
+export default MySearch
